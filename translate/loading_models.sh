b8c887b3144631e6fbe7fdb5b32b69dd66a4ae10
gamallo <pablo.gamallo@gmail.com>
2022-05-11 00:55:36 +0200

    updated

--- translate/loading_models.sh
+++ translate/loading_models.sh
@@ -3,6 +3,8 @@
 chmod 0755 trans.sh
 mkdir tmp
 
+rm models_trans_gl.tgz
+
 echo "Dowload models trained for galician"
 
 wget -nc http://fegalaz.usc.es/~gamallo/resources/models_trans_gl.tgz
--- translate/trans.sh
+++ translate/trans.sh
@@ -78,20 +78,32 @@ echo $input > ./input_output/input.txt
 input_file="./input_output/input.txt"
 
 if [ "$lang1" == "es" ]  || [ "$lang1" == "en" ]  && [ "$lang2" == "gl" ]  && [ "$system" == "lstm" ] ; then
+   echo "Classic tokenization of the input text"
+   cat  $input_file | ./tokenizer.perl > __temp ; mv __temp $input_file
+   
    echo "Translating " $lang1"->"$lang2 " with "$system
    onmt_translate -model $ModelLSTM"/"$lang1"-"$lang2".lstm" -src $input_file -output $output_file -gpu -1 -verbose -replace_unk
+
+   echo "Detokenizing the output text"
+   cat  $output_file | ./detokenizer.perl > __temp ; mv __temp $output_file
 fi
 
 if [ "$lang1" == "es" ] || [ "$lang1" == "en" ]  && [ "$lang2" == "gl" ]  && [ "$system" == "transf" ] ; then
    
+   echo "Classic tokenization of the input text"
+   cat  $input_file | ./tokenizer.perl > __temp ; mv __temp $input_file
+   
    echo "Tokenizing " $lang1 "with sentencepiece"
-   python $LIB/spm_encode.py --model=$ModelSP"/"${lang1}"-"${lang2}"."$lang1".sp" < $input_file > ./tmp/_sp_$lang1
+   python3 $LIB/spm_encode.py --model=$ModelSP"/"${lang1}"-"${lang2}"."$lang1".sp" < $input_file > ./tmp/_sp_$lang1
 	
    echo "Translating " $lang1"->"$lang2 " with "$system
    onmt_translate  -gpu -1 -batch_size 16384 -batch_type tokens -beam_size 5 -model $ModelTransformer"/"$lang1"-"$lang2".transf"  -src ./tmp/_sp_$lang1  -output ./tmp/_sp_$lang2 
    
    echo "decoding output"
-   python $LIB/spm_decode.py --model=$ModelSP"/"${lang1}"-"${lang2}"."$lang2".sp" --input_format=piece < ./tmp/_sp_$lang2 > $output_file
+   python3 $LIB/spm_decode.py --model=$ModelSP"/"${lang1}"-"${lang2}"."$lang2".sp" --input_format=piece < ./tmp/_sp_$lang2 > $output_file
+
+   echo "Detokenizing the output text"
+   cat  $output_file | ./detokenizer.perl > __temp ; mv __temp $ouput_file
 
 fi
 
